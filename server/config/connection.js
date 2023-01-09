70dc9e8a21d30eeeabfddc2bd3db3915e124548c
SSimonPhd <simon_phd@protonmail.com>
2023-01-09 01:43:09 -0600

    Refactored config files

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping', {
+//|| 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 module.exports = mongoose.connection;
